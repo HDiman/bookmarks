00982bef2bdeefd4e9823352f7ddddf90117ced6
Dmitry Sannikov <HDiman@users.noreply.github.com>
2024-01-28 23:57:09 +0300

    Add registration

--- bookmarks/account/views.py
+++ bookmarks/account/views.py
@@ -29,9 +29,7 @@ def user_login(request):
 
 @login_required
 def dashboard(request):
-    return render(request,
-                  'account/dashboard.html',
-                  {'section': 'dashboard'})
+    return render(request, 'account/dashboard.html', {'section': 'dashboard'})
 
 
 def register(request):
@@ -42,15 +40,10 @@ def register(request):
             # но пока не сохранять его
             new_user = user_form.save(commit=False)
             # Установить выбранный пароль
-            new_user.set_password(
-                user_form.cleaned_data['password'])
+            new_user.set_password(user_form.cleaned_data['password'])
             # Сохранить объект User
             new_user.save()
-            return render(request,
-                          'account/register_done.html',
-                          {'new_user': new_user})
+            return render(request, 'account/register_done.html', {'new_user': new_user})
     else:
         user_form = UserRegistrationForm()
-    return render(request,
-                  'account/register.html',
-                  {'user_form': user_form})
+    return render(request, 'account/register.html', {'user_form': user_form})
