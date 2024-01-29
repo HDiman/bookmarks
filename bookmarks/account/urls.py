a5783f24379b96509fa03aad5457fea413d30fc7
Dmitry Sannikov <HDiman@users.noreply.github.com>
2024-01-29 22:36:58 +0300

    Send messages

--- bookmarks/account/urls.py
+++ bookmarks/account/urls.py
@@ -16,8 +16,8 @@ urlpatterns = [
     # path('password-change/done/', auth_views.PasswordChangeDoneView.as_view(), name='password_change_done'),
 
     # url-адреса сброса пароля
-    # path('password-reset/', auth_views.PasswordResetView.as_view(), name='password_reset'),
-    # path('password-reset/done/', auth_views.PasswordResetDoneView.as_view(), name='password_reset_done'),
+    # path('password-reset/', auth_views.PasswordResetView.as_view(template_name='account/templates/registration/password_reset_form.html'), name='password_reset'),
+    # path('password-reset/done/', auth_views.PasswordResetDoneView.as_view(template_name='account/templates/registration/password_reset_done.html'), name='password_reset_done'),
     # path('password-reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
     # path('password-reset/complete/', auth_views.PasswordResetCompleteView.as_view(), name='password_reset_complete'),
 
