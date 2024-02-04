f87bb1879187ad76925c52b3d456dd84cfb866b9
Dmitry Sannikov <HDiman@users.noreply.github.com>
2024-02-05 00:57:38 +0300

    Work with bookmarklet 2

---
+++ bookmarks/images/templates/bookmarklet_ launcher.js
@@ -0,0 +1,20 @@
+//(function(){
+//    if(!window.bookmarklet) {
+//        bookmarklet_js = document.body.appendChild(document.createElement('script'));
+//        bookmarklet_js.src = 'http://127.0.0.1:8000/static/js/bookmarklet.js?r='+Math.floor(Math.random()*9999999999999999);
+//        window.bookmarklet = true;
+//        }
+//    else {
+//        bookmarkletLaunch();
+//    }
+//})();
+
+(function(){
+    if (window.myBookmarklet !== undefined){
+￼    	myBookmarklet();
+	}
+
+    else {
+           document.body.appendChild(document.createElement('script')).src='http://127.0.0.1:8000/static/js/bookmarklet.js?r='+Math.floor(Math.random()*99999999999999999999);
+	}
+})();
